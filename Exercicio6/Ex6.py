ae804ca638032592c2d7b755a52d3c39c0f55f6c
John <jonathan.mota@outlook.com>
2018-10-09 21:35:56 -0300

    ExProceduralAtualizado

--- Exercicio6/Ex6.py
+++ Exercicio6/Ex6.py
@@ -4,3 +4,11 @@
 # Ex: todosnimparesQ([1,3,5,7]) = True
 # Ex: todosnimparesQ([]) = True
 # Ex: todosnimparesQ([1,2,3,4,5]) = False
+
+
+def todos_imparesQ(w):
+    return True if len(w) == 0 or (w[len(w) - 1] % 2 != 0 and todos_imparesQ(w[:-1])) else False
+
+print(todos_imparesQ([1,1,3,7,11]))
+print(todos_imparesQ([]))
+print(todos_imparesQ([2,4,6,8,10]))
