d41998e4b9aec15be62d2af6a308a17e385c03b1
John <jonathan.mota@outlook.com>
2018-10-28 00:03:01 -0300

    Decorator Com@Funcoes

--- Exercicio8/Com@Funcoes/Ex4.py
+++ Exercicio8/Com@Funcoes/Ex4.py
@@ -5,3 +5,54 @@
 # pode fcar indefnidamente escolhendo adicionais: 1 para chocolate, 2 para espuma
 # de leite, 3 para caramelo e 4 para canela. A cada adicional escolhido, decore o objeto
 # café. No fnal, mostre o preço total.
+
+def palitos_de_chocolate(f):
+    def decorator():
+        return f() + 0.5
+    return decorator
+
+def espuma_de_leite(f):
+    def decorator():
+        return f() + 0.2
+    return decorator
+
+def caramelo(f):
+    def decorator():
+        return f() + 0.1
+    return decorator
+
+def canela(f):
+    def decorator():
+        return f() + 0.3
+def cafe():
+    return 5.0
+def opcoes():
+    return int(
+                input("1 - Adicionar Palitos de chocolate \n" + "2 - Adicionar Espuma de leite \n" +"3 - Adicionar Caramelo \n" +"4 - Adicionar Canela \n" +"0 - Sair \n"
+                )
+            )
+
+tipo = opcoes()
+while (tipo != 0):
+    if tipo == 1:
+        print("Palitos de chocolate no seu café")
+        @palitos_de_chocolate
+        def cafe():
+            return 5.0
+    if tipo == 2:
+        print("Espuma de leite no seu café!")
+        @espuma_de_leite
+        def cafe():
+            return 5.0
+    if tipo == 3:
+        print("Caramelo no seu café!")
+        @caramelo
+        def cafe():
+            return 5.0
+    if tipo == 4:
+        print("Canela no seu café!")
+        @canela
+        def cafe():
+            return 5.0
+    tipo = opcoes()
+print("Valor Total:", cafe())
